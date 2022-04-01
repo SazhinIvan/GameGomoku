2fc7798ecb12cc97e1176a03528ce0520da0648a
IDSazhin <sazhin77@gmail.com>
2022-04-01 11:27:49 +0300

    измнение курсора мыши

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,5 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Windows.Forms;
+
+using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Reflection;
+
+
 namespace WindowsFormsApp1
 {
     public class button_x
@@ -7,6 +14,24 @@ namespace WindowsFormsApp1
         public int m { get; set; }
         public int n { get; set; }
         public System.Windows.Forms.Button _button { get; set; }
+    }
+
+
+ 
+static class NativeMethods
+    {
+        public static Cursor LoadCustomCursor(string path)
+        {
+            IntPtr hCurs = LoadCursorFromFile(path);
+            if (hCurs == IntPtr.Zero) throw new Win32Exception();
+            var curs = new Cursor(hCurs);
+            // Note: force the cursor to own the handle so it gets released properly
+            var fi = typeof(Cursor).GetField("ownHandle", BindingFlags.NonPublic | BindingFlags.Instance);
+            fi.SetValue(curs, true);
+            return curs;
+        }
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern IntPtr LoadCursorFromFile(string path);
     }
 
     partial class Form1
@@ -40,6 +65,7 @@ namespace WindowsFormsApp1
 
             this.Width = (size_pole + 1) * 50;
             this.Height = (size_pole + 1) * 50;
+            this.Cursor = NativeMethods.LoadCustomCursor(@"D:/cursorm.cur");
             
             //_buttons = new Button[width_x * height_y];
 
